740b69fd8c4834e9dd39ff2343038366ed6f482f
Christopher Neal <christopher.m.neal@gmail.com>
2021-12-13 15:21:49 -0800

    BUGFIX: result.erb view now displaying
    Co-authored-by: Isaac Overstreet <isaacoverstreet3@gmail.com>

--- app.rb
+++ app.rb
@@ -5,28 +5,14 @@ require('pry')
 also_reload('lib/**/*.rb')
 
 get('/') do
-  # @triangle = Triangle.all
   erb(:triangle)
 end
 
-get('/triangle') do
-  # @triangle = Triangle.all
-  erb(:triangle)
-end
-
-post('/triangle') do
-  erb(:result)
-end
-
-get('/result') do
+post('/result') do
   side1 = params[:side1]
   side2 = params[:side2]
   side3 = params[:side3]
   triangle = Triangle_Checker.new(side1, side2, side3)
-  triangle.triangle_checker()
+  @result = triangle.triangle_checker()
+  erb(:result)
 end
-
-# get('/albums/:id') do
-#   @album = Album.find(params[:id].to_i())
-#   erb(:album)
-# end
